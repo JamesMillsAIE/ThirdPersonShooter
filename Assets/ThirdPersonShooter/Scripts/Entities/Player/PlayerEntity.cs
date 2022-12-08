8634e1fc27ec4dc96f70e5bdca98b26d4dc99dd2
James Mills <james.mills@aie.edu.au>
2022-12-08 16:48:57 +1100

    day 2

--- Assets/ThirdPersonShooter/Scripts/Entities/Player/PlayerEntity.cs
+++ Assets/ThirdPersonShooter/Scripts/Entities/Player/PlayerEntity.cs
@@ -1,4 +1,6 @@
-﻿using ThirdPersonShooter.UI;
+﻿using System;
+
+using ThirdPersonShooter.UI;
 
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -10,24 +12,44 @@ namespace ThirdPersonShooter.Entities.Player
 		public ref Stats Stats => ref stats;
 		public Vector3 Position => transform.position;
 
+		public Action<int> onScoreUpdated;
+
 		[SerializeField] private Stats stats;
 		[SerializeField] private InputActionReference pauseAction;
 
 		[Header("Components")] 
 		[SerializeField] private PlayerInput input;
-
+		[SerializeField] private Weapon weapon;
 		[SerializeField] private AudioSource hurtSource;
 		[SerializeField] private AudioSource deathSource;
+
+		private int score;
+
+		public void AddScore(int _value)
+		{
+			score += _value;
+			onScoreUpdated?.Invoke(score);
+		}
 
 		private void Awake()
 		{
 			stats.Start();
+			stats.onDeath += OnDeath;
+			stats.onHealthChanged += OnHealthChanged;
+			
+			weapon.SetPlayer(this);
 
 			if(GameManager.IsValid())
 				GameManager.Instance.Player = this;
 
 			if(UIManager.IsValid())
 				input.uiInputModule = UIManager.Instance.inputModule;
+		}
+
+		private void OnDestroy()
+		{
+			stats.onDeath -= OnDeath;
+			stats.onHealthChanged -= OnHealthChanged;
 		}
 
 		private void OnEnable() => pauseAction.action.performed += OnPausePerformed;
@@ -39,5 +61,9 @@ namespace ThirdPersonShooter.Entities.Player
 			if(GameManager.IsValid())
 				GameManager.Instance.TogglePaused();
 		}
+
+		private void OnDeath() => deathSource.Play();
+
+		private void OnHealthChanged(float _health) => hurtSource.Play();
 	}
 }
