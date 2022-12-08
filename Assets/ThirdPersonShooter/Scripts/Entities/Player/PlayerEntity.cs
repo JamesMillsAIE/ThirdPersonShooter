c3d98d3bde13579e9b822709a5d5f437dd9e2907
James Mills <james.mills@aie.edu.au>
2022-12-08 11:02:33 +1100

    ui polish and general fixes

--- Assets/ThirdPersonShooter/Scripts/Entities/Player/PlayerEntity.cs
+++ Assets/ThirdPersonShooter/Scripts/Entities/Player/PlayerEntity.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿using ThirdPersonShooter.UI;
 
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace ThirdPersonShooter.Entities.Player
 {
@@ -10,10 +11,33 @@ namespace ThirdPersonShooter.Entities.Player
 		public Vector3 Position => transform.position;
 
 		[SerializeField] private Stats stats;
+		[SerializeField] private InputActionReference pauseAction;
+
+		[Header("Components")] 
+		[SerializeField] private PlayerInput input;
+
+		[SerializeField] private AudioSource hurtSource;
+		[SerializeField] private AudioSource deathSource;
 
 		private void Awake()
 		{
 			stats.Start();
+
+			if(GameManager.IsValid())
+				GameManager.Instance.Player = this;
+
+			if(UIManager.IsValid())
+				input.uiInputModule = UIManager.Instance.inputModule;
+		}
+
+		private void OnEnable() => pauseAction.action.performed += OnPausePerformed;
+
+		private void OnDisable() => pauseAction.action.performed -= OnPausePerformed;
+
+		private void OnPausePerformed(InputAction.CallbackContext _context)
+		{
+			if(GameManager.IsValid())
+				GameManager.Instance.TogglePaused();
 		}
 	}
 }
